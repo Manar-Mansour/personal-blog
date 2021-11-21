e67e9a84554d2e13bf9f6be4fe00311529ed1c50
Manar <manar.abdelgalil2014@gmail.com>
2021-11-21 02:23:13 +0200

    ready

---
+++ build/precache-manifest.141e2b51ccd35c38159f4d2c643a8e55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e33da89ba6ae83210ea7",
+    "url": "/static/css/main.bed6eed7.chunk.css"
+  },
+  {
+    "revision": "e33da89ba6ae83210ea7",
+    "url": "/static/js/main.0f2525d2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "61c4c899622b805a72bd",
+    "url": "/static/js/2.dffe7372.chunk.js"
+  },
+  {
+    "revision": "e7b3783da5e7ad2ac731edbc0afcd94e",
+    "url": "/index.html"
+  }
+];
